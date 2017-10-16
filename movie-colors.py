ba65e11747c7fc3c0a3c1f751072f165cea1cc86
aDrz <adrien.nouvellet@gmail.com>
2017-10-16 15:26:18 +0200

    .

--- movie-colors.py
+++ movie-colors.py
@@ -7,9 +7,10 @@ import sys
 import argparse
 from sklearn.cluster import KMeans
 import scipy
+import pickle
 
 def get_kmeans(img, n_clusters, n_jobs=6):
-    model = KMeans(n_clusters=n_clusters, n_jobs=6)
+    model = KMeans(n_clusters=n_clusters, n_jobs=n_jobs)
     model.fit_predict(img)
     return model.cluster_centers_
 
@@ -18,20 +19,25 @@ def process_movie(file_path=''):
     Process movie file
     '''
     cap = cv2.VideoCapture(file_path)
-
-    cnt = 0
+    n_imgs = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cnt_total = 0
+    cnt = 1
     list_centers = []
     while cap.isOpened():
-        success, img = cap.read()
+        while cnt%10:
+            success, img = cap.read()
+            cnt+=1
+            cnt_total+=1
+        cnt=1
         img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         img_hsv = scipy.misc.imresize(img_hsv, .1)
         img_hsv = img_hsv.reshape(img_hsv.shape[0]*img_hsv.shape[1], img_hsv.shape[2])
         list_centers.append(get_kmeans(img_hsv, 3, 6))
-        cnt+=1
-        print(cnt)
+        print(cnt_total/n_imgs*100)
         if cnt>1000:
             break
     cap.release()
+    pickle.dump(list_centers, open('data.p','wb'))
 
 def main(argv):
     parser = argparse.ArgumentParser()
