87906fdccf8d09a522c6cf29f57efd88ac776786
aDrz <adrien.nouvellet@gmail.com>
2017-10-16 18:43:34 +0200

    .

--- movie-colors.py
+++ movie-colors.py
@@ -24,8 +24,6 @@ def get_kmeans_cv(img, n_clusters=3):
 
 
 def get_kmeans_cuda(img, n_clusters=3):
-#    centroids, assignments, avg_distance = kmeans_cuda(
-    #    arr, 4, metric="cos", verbosity=1, seed=3, average_distance=True)
     centroids, assignments = kmeans_cuda(np.float32(img), n_clusters, verbosity=0, yinyang_t=0)
     return centroids
 
@@ -43,16 +41,16 @@ def process_movie(file_path='', alg='cv'):
     while cap.isOpened():
         while cnt%10:
             success, img = cap.read()
-            if success==False:
-                break
             cnt+=1
             cnt_total+=1
         cnt=1
-        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        if success:    
+            img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        else:
+            break
         r = .1
         dim = (int(img_hsv.shape[1]*r), int(img_hsv.shape[0] * r))
         img_hsv = cv2.resize(img_hsv, dim, interpolation=cv2.INTER_AREA)
-        
         img_hsv = img_hsv.reshape(img_hsv.shape[0]*img_hsv.shape[1], img_hsv.shape[2])
         if alg=='cv':
             list_centers.append(get_kmeans_cv(img_hsv, 3))
