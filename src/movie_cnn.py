bdc71ec77f2b9606784ee467c7ab21ed6a9d11cf
adrien <adrien.nouvellet@gmail.com>
2018-05-03 10:25:05 +0200

    clean code

--- src/movie_cnn.py
+++ src/movie_cnn.py
@@ -3,7 +3,8 @@
 
 import cv2
 import numpy as np
-import sys,os
+import sys
+import os
 import argparse
 import pickle
 import matplotlib.pyplot as plt
@@ -29,7 +30,8 @@ def cov(X):
     """
     return dot(X.T, X) / X.shape[0]
 
-def pca(data, pc_count = None):
+
+def pca(data, pc_count=None):
     """
     Principal component analysis using eigenvalues
     note: this mean-centers and auto-scales the data (in-place)
@@ -45,17 +47,18 @@ def pca(data, pc_count = None):
 
 
 def variance_of_laplacian(image):
-	# compute the Laplacian of the image and then return the focus
+        # compute the Laplacian of the image and then return the focus
     # measure, which is simply the variance of the Laplacian
     # credit: https://www.pyimagesearch.com/2015/09/07/blur-detection-with-opencv/
     return cv2.Laplacian(image, cv2.CV_64F).var()
+
 
 def get_features(list_img_path):
     model = VGG19(weights='imagenet', include_top=False)
     features = []
     n_images = len(list_img_path)
     for idx, img_path in enumerate(list_img_path):
-        print('getting features for %s %d/%d'%
+        print('getting features for %s %d/%d' %
               (img_path, idx+1, n_images))
         # Resize image to be 224x244
         img = image.load_img(img_path, target_size=(224, 224))
@@ -63,7 +66,7 @@ def get_features(list_img_path):
         x = np.expand_dims(x, axis=0)
         x = preprocess_input(x)
         y = model.predict(x)
-        # Vectorize the 7x7x512 tensor                                                           
+        # Vectorize the 7x7x512 tensor
         y = y.reshape(reduce(mul, y.shape, 1))
         features.append(y)
     return (list_img_path, features)
@@ -80,9 +83,9 @@ def process_movie(file_path, path_img='imgs'):
     img_list_gray = []
     list_img_path = []
     while cap.isOpened():
-        while cnt%24:
+        while cnt % 24:
             success, img = cap.read()
-            if success==False:
+            if not success:
                 return list_img_path
             r = 0.5
             dim = (int(img.shape[1]*r), int(img.shape[0] * r))
@@ -91,14 +94,14 @@ def process_movie(file_path, path_img='imgs'):
             img_list.append(img)
             img_list_gray.append(img_gray)
 #            cv2.imwrite("img/blurry/img-%d.jpg"%cnt_total, img)
-            cnt+=1
-            cnt_total+=1
+            cnt += 1
+            cnt_total += 1
 
         cnt = 1
         blurness = []
         blurness = [variance_of_laplacian(img) for img in img_list_gray]
         max_idx = np.argmax(blurness)
-        img_p = "%s/img-%d.jpg"%(path_img, cnt_total)
+        img_p = "%s/img-%d.jpg" % (path_img, cnt_total)
         cv2.imwrite(img_p, img_list[max_idx])
         list_img_path.append(img_p)
         img_list = []
@@ -108,14 +111,15 @@ def process_movie(file_path, path_img='imgs'):
 
 
 def tsne_to_grid(data, output):
-    #https://github.com/Quasimondo/RasterFairy/blob/master/examples/Raster%20Fairy%20Demo%201.ipynb
+    # https://github.com/Quasimondo/RasterFairy/blob/master/examples/Raster%20Fairy%20Demo%201.ipynb
     # https://github.com/bmcfee/RasterFairy (vpython3)
 
-#    from sklearn.decomposition import PCA
-    import rasterfairy, math
+    #    from sklearn.decomposition import PCA
+    import rasterfairy
+    import math
 
     data = np.array(data)
-    
+
 #    pca = PCA(n_components=500, whiten=True)
     n_pts = data.shape[0]
     #    data_pca = pca.fit_transform(data)
@@ -124,15 +128,14 @@ def tsne_to_grid(data, output):
     data_pca = list(data_pca)
     sqrt_npts = math.ceil(math.sqrt(n_pts))
     n_diff = sqrt_npts**2 - n_pts
-    data_pca+=[data_pca[-1]]*n_diff
+    data_pca += [data_pca[-1]]*n_diff
     data_pca = np.array(data_pca)
-    list_file+=[list_file[-1]]*n_diff
+    list_file += [list_file[-1]]*n_diff
 
     tsne = TSNE_multi(n_components=2, perplexity=50, n_jobs=8)
     data_tsne = tsne.fit_transform(data_pca.astype(np.float64))
 
     arrangements = rasterfairy.getRectArrangements(data_tsne.shape[0])
-
 
     img = cv2.imread(list_file[0])
     ratio = img.shape[0]/img.shape[1]
@@ -141,20 +144,21 @@ def tsne_to_grid(data, output):
     img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
     dim_x = img.shape[0]
     dim_y = img.shape[1]
-    
-    grid_xy, (width, height) = rasterfairy.transformPointCloud2D(data_tsne,target=arrangements[0])
+
+    grid_xy, (width, height) = rasterfairy.transformPointCloud2D(
+        data_tsne, target=arrangements[0])
 
     img_big = np.zeros((dim_x*width, dim_y*height, 3))
 
     for i, file_img in enumerate(list_file):
-        xy = grid_xy[i,:]
+        xy = grid_xy[i, :]
         pos_x = int(xy[0]*dim_x)
         pos_y = int(xy[1]*dim_y)
         img = cv2.imread(file_img)
         r = 0.1
         dim = (int(img.shape[1]*r), int(img.shape[0] * r))
         img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-        img_big[pos_x:pos_x+dim_x, pos_y:pos_y+dim_y,:] = img
+        img_big[pos_x:pos_x+dim_x, pos_y:pos_y+dim_y, :] = img
     cv2.imwrite(output, img_big)
 
 
